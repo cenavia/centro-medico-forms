1c6a07aef394d0e2a3da7150902bce763335fe46
Carlos Navia <cenaviat@gmail.com>
2023-08-10 16:04:28 -0500

    creacion de campos de formularios y obtencion de datos de los campos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,11 +3,15 @@ import { useState } from 'react';
 export const Formulario = () => {
     
     const [nombreMascota, setNombreMascota] = useState('');
+    const [propietario, setPropietario] = useState('');
+    const [email, setEmail] = useState('');
+    const [fecha, setFecha] = useState('');
+    const [sintomas, setSintomas] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log(nombreMascota);
+
     } 
     
     return (
@@ -37,6 +41,61 @@ export const Formulario = () => {
                         onChange={ (e) => setNombreMascota(e.target.value) }
                     />  
                 </div>
+
+                <div className="mb-5">
+                    <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
+                        Nombre Propietario
+                    </label>
+                    <input
+                        id="propietario"
+                        type="text"
+                        placeholder="Nombre del Propietario"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={propietario}
+                        onChange={ (e) => setPropietario(e.target.value) }
+                    />  
+                </div>
+
+                <div className="mb-5">
+                    <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Email Contacto Propietario"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={email}
+                        onChange={ (e) => setEmail(e.target.value) }
+                    />  
+                </div>
+
+                <div className="mb-5">
+                    <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">
+                        Alta
+                    </label>
+                    <input
+                        id="alta"
+                        type="date"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={fecha}
+                        onChange={ (e) => setFecha(e.target.value) }
+                    />  
+                </div>
+
+                <div className="mb-5">
+                    <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
+                        Síntomas
+                    </label>
+                    <textarea 
+                        id="sintomas"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        placeholder="Describe los Síntomas"
+                        value={sintomas}
+                        onChange={ (e) => setSintomas(e.target.value) }
+                    />
+                </div>
+
                 <input
                     type="submit"
                     className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
